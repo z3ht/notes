769795b072c63d4479ebf8a87e1aadb30f4b11fb
z3ht <andr3w.darling@gmail.com>
2020-05-13 00:59:19 -0600

    Continue building a house price estimating machine learning algorithm

--- ml/hands_on_ml/examples/personal/retrieve.py
+++ ml/hands_on_ml/examples/personal/retrieve.py
@@ -2,6 +2,8 @@ import os
 import tarfile
 import urllib.request
 import pandas as pd
+import numpy as np
+from zlib import crc32
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
 
@@ -17,3 +19,21 @@ def extract_tar(url, path, tar_name):
 def load_csv(path, name):
     csv_path = os.path.join(path, name)
     return pd.read_csv(csv_path)
+
+
+def test_set_check(id_, test_ratio=0.2):
+                                # Below converts from signed to unsigned
+                                # Necessary because lib was changed in python 3.0
+    return crc32(np.int64(id_)) & 0xffffffff < (test_ratio * 2**32)
+
+
+def split_dataset_by_id(data, test_ratio=0.2, id_column="index"):
+    if id_column == "index":
+        print("WARNING:: You are using the index column as your spliting ID.")
+        print("          This is dangerous. Be sure to never remove a column")
+        print("          or add a new row without setting its index. Consider")
+        print("          using a more stable column for your id")
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
